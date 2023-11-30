27dba96865db13aca19f0767952d72463db6bbd6
Anurag Tarai <146613679+Anurag-Tarai@users.noreply.github.com>
2023-11-30 23:33:14 +0000

    create a new file

---
+++ basics/L4_variables.js
@@ -0,0 +1,8 @@
+const accountId = 144553
+let accountEmail = "anurag4848@gamil.com"
+var accountPassword = "1234"
+accountCity = "jaipur"
+
+// accountId = 2 // this is not allowed
+
+console.log(accountId);
