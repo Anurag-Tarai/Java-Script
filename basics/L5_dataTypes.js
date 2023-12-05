4921880adb798e97c92a7736d137dbf69d0f0ae4
Anurag Tarai <146613679+Anurag-Tarai@users.noreply.github.com>
2023-12-05 15:20:13 +0000

    lecture 5 is finished !

---
+++ basics/L5_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+// alert (3+3) //we are using nodejs, not browser
+
+// data types
+let name = "anurag"
+
+let age = 18
+
+let isLoggedIn = false // this boolean type
+
+// number 
+// bigint
+// string =>"" or '' but prefer ""
+// boolean=> true/false
+// null => standlone value (represent empty)
+// undefined => the value is not assigned yet
+// symbol => uniqueness 
+// object
+
+console.log(typeof "ANURAG");
+ 
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
