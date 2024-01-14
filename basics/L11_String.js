3e29e4483875ecc40fd5eb67e3bff0955aeb4f2d
Anurag Tarai <146613679+Anurag-Tarai@users.noreply.github.com>
2024-01-14 05:43:01 +0000

    L11 upto 11 min

--- basics/L11_String.js
+++ basics/L11_String.js
@@ -4,9 +4,20 @@ const repoCount= 20
 console.log(name + repoCount+ 'tarai');// this method for concatenate is old // outDated and not recommended
 
 // latest way=================================
-console.log(`${name}${repoCount}${'tarai'}`);// use of back ticks and place holder// sting interpolation
-console.log(`name is ${name} and repo count is ${repoCount}`);
+console.log(`${name}${repoCount}${'tarai'}`);// use of back ticks and place holder// string interpolation
+console.log(`name is ${name} and repo count is ${repoCount}`);// more readable, reliable
 
-const gameName = new String('pubg')// another way to declare string
+const gameName = new String('pubg')// another way to declare string 
 
-console.log(typeof String);
+console.log(typeof gameName);// object
+
+console.log(gameName [0]);//p // see the sreen short in ipad js folder L11 about prototype
+console.log(gameName.__proto__);// {}
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());// PUBG // this does not change the original value ---> L10 stack and heap
+console.log(gameName);// [String: 'pubg'] 
+
+console.log(gameName.charAt(3));// g
+console.log(gameName.indexOf('g'));// 3
+
