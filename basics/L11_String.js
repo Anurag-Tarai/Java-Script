5c5f80c3147ee48dfef341e57797819460492b7e
Anurag Tarai <146613679+Anurag-Tarai@users.noreply.github.com>
2024-01-15 17:24:16 +0000

    L11 completed !!

--- basics/L11_String.js
+++ basics/L11_String.js
@@ -1,23 +1,47 @@
-const name = 'anurag'
-const repoCount= 20
+// const name = 'anurag'
+// const repoCount= 20
 
-console.log(name + repoCount+ 'tarai');// this method for concatenate is old // outDated and not recommended
+// console.log(name + repoCount+ 'tarai');// this method for concatenate is old // outDated and not recommended
 
-// latest way=================================
-console.log(`${name}${repoCount}${'tarai'}`);// use of back ticks and place holder// string interpolation
-console.log(`name is ${name} and repo count is ${repoCount}`);// more readable, reliable
+// morden way=================================
+// console.log(`${name}${repoCount}${'tarai'}`);// use of back ticks and place holder// string interpolation
+// console.log(`name is ${name} and repo count is ${repoCount}`);// more readable, reliable
 
-const gameName = new String('pubg')// another way to declare string 
+// const gameName = new String('pubg')// another way to declare string 
 
-console.log(typeof gameName);// object
+// console.log(typeof gameName);// object
 
-console.log(gameName [0]);//p // see the sreen short in ipad js folder L11 about prototype
-console.log(gameName.__proto__);// {}
+// string prototype and method=========================================// mdn docuement in detail// very important, become familiar with string methods
+// console.log(gameName [0]);//p // see the sreen short in ipad js folder L11 about prototype
+// console.log(gameName.__proto__);// {}
 
-console.log(gameName.length);
-console.log(gameName.toUpperCase());// PUBG // this does not change the original value ---> L10 stack and heap
-console.log(gameName);// [String: 'pubg'] 
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());// PUBG // this does not change the original value ---> L10 stack and heap
+// console.log(gameName);// [String: 'pubg'] 
 
-console.log(gameName.charAt(3));// g
-console.log(gameName.indexOf('g'));// 3
+// console.log(gameName.charAt(3));// g // string methods
+// console.log(gameName.indexOf('g'));// 3
+
+// const NewString = gameName.substring(0,3)// Not include last position // no -ve value
+// console.log(NewString);// pub
+
+// const anotherString = gameName.slice(-3,3)// start from negative
+// console.log(anotherString);// ub
+
+// const string1 = '  anurag  '
+// console.log(string1);
+// console.log(string1.trim());// remove spaces // mdn document read more
+
+// const url = 'http://anurag.com/tarai%20odisha'
+
+// console.log(url.replace('%20','**'));// http://anurag.com/tarai**odisha
+// console.log(url.includes('anur'));// true // to findout value present in the string or not
+
+// const string2 = 'anurag-tarai-mca-vssut'
+// console.log(string2.split('-'));// slipt into array
+
+//==> you need familiar with string methods
+
+
+
 
---
+++ basics/rough.js
@@ -0,0 +1,6 @@
+let number = 5;        // This is an integer
+
+
+let result = String(number);
+console.log(result);
+console.log(typeof result);  // Output: 7.5
