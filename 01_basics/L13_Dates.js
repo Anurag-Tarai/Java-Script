6081173e511e796a5e60b97a05bfca057c4761c2
Anurag Tarai <anuragtarai051@gmail.com>
2024-03-20 23:37:21 +0530

    Lecture 27 and 28 finished and some changes in folder names !

---
+++ 01_basics/L13_Dates.js
@@ -0,0 +1,41 @@
+// -- -- -- -- -- -- --DATES-- -- -- -- -- -- --
+
+// let myDate = new Date()
+
+// console.log(myDate);// 2024-02-16T15:29:46.096Z
+// console.log(myDate.toString());// Fri Feb 16 2024 21:01:20 GMT+0530 (India Standard Time)
+// console.log(myDate.toDateString());//Fri Feb 16 2024
+// console.log(myDate.toISOString());// 2024-02-16T15:34:44.695Z
+// console.log(myDate.toJSON());// 2024-02-16T15:34:44.695Z
+// console.log(myDate.toLocaleDateString());// 16/2/2024
+// console.log(myDate.toTimeString());// 21:04:44 GMT+0530 (India Standard Time)
+// console.log(myDate.toLocaleString());//16/2/2024, 9:09:56 pm   
+
+// // typeof date---------------
+// console.log(typeof(myDate));// object
+
+// let CreateDate = new Date(2024,2,12)
+// // months start from 0 in java sript
+
+// console.log(CreateDate.toDateString());// Tue Mar 12 2024
+// let CreateDate1 = new Date(2024,2,12,5,3)
+// let CreateDate2 = new Date("2023-01-15")
+// let CreateDate3 = new Date("01-14-2023")
+//------------------------------
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);// 1708099330032 in mili second
+// console.log(CreateDate.getTime());// 1710181800000 in mili second
+
+// console.log(Math.floor(Date.now()/1000));// to convert mili second into second
+//--------------------------------
+// let newDate = new Date()
+// console.log(newDate);// 2024-02-16T16:10:17.487Z
+// console.log(newDate.toDateString());// Fri Feb 16 2024
+// console.log(newDate.getMonth()+1);// 2
+// console.log(newDate.getDate());// 16
+// console.log(newDate.getDay());// 5
+// console.log(newDate.getTime());// 1708099817487
+// ----------------------------------
+// used to print complex date for end user
+ 
+
